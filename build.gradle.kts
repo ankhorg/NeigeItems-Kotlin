a24eaa4685b19bfa2972ae3c4435eddfb77a566b
Neige <2468629609@qq.com>
2022-07-09 16:27:41 +0800

    project create

---
+++ build.gradle.kts
@@ -0,0 +1,68 @@
+plugins {
+    `java-library`
+    `maven-publish`
+    id("io.izzel.taboolib") version "1.40"
+    id("org.jetbrains.kotlin.jvm") version "1.5.10"
+}
+
+taboolib {
+    install("common")
+    install("common-5")
+    install("module-chat")
+    install("module-kether")
+    install("module-metrics")
+    install("module-nms")
+    install("module-nms-util")
+    install("platform-bukkit")
+    classifier = null
+    version = "6.0.9-25"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("ink.ptms:nms-all:1.0.0")
+    compileOnly("ink.ptms.core:v11900:11900-minimize:mapped")
+    compileOnly("ink.ptms.core:v11900:11900-minimize:universal")
+    compileOnly(kotlin("stdlib"))
+    compileOnly(fileTree("libs"))
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = listOf("-Xjvm-default=all")
+    }
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://repo.tabooproject.org/repository/releases")
+            credentials {
+                username = project.findProperty("taboolibUsername").toString()
+                password = project.findProperty("taboolibPassword").toString()
+            }
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("library") {
+            from(components["java"])
+            groupId = project.group.toString()
+        }
+    }
+}
---
+++ src/main/kotlin/pers/neige/neigeitems/NeigeItems.kt
@@ -0,0 +1,11 @@
+package pers.neige.neigeitems
+
+import taboolib.common.platform.Plugin
+import taboolib.common.platform.function.info
+
+object NeigeItems : Plugin() {
+
+    override fun onEnable() {
+        info("Successfully running ExamplePlugin!")
+    }
+}
