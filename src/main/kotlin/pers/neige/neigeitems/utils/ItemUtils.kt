850491fa33b37238cc957975bbbfe4da0b3f34aa
Neige <2468629609@qq.com>
2022-07-11 19:29:35 +0800

    update

--- src/main/kotlin/pers/neige/neigeitems/utils/ItemUtils.kt
+++ src/main/kotlin/pers/neige/neigeitems/utils/ItemUtils.kt
@@ -41,11 +41,27 @@ object ItemUtils {
         }
     }
 
+    // 类型强制转换
+    @JvmStatic
+    fun String.castToItemTagData(): ItemTagData {
+        return when {
+            this.startsWith("(Byte) ") -> ItemTagData(this.substring(7, this.length).toByte())
+            this.startsWith("(Short) ") -> ItemTagData(this.substring(8, this.length).toShort())
+            this.startsWith("(Int) ") -> ItemTagData(this.substring(6, this.length).toInt())
+            this.startsWith("(Long) ") -> ItemTagData(this.substring(7, this.length).toLong())
+            this.startsWith("(Float) ") -> ItemTagData(this.substring(8, this.length).toFloat())
+            this.startsWith("(Double) ") -> ItemTagData(this.substring(9, this.length).toDouble())
+            else -> ItemTagData(this)
+        }
+    }
+
     // 转 ItemTagData
     @JvmStatic
     fun Any.toItemTagData(): ItemTagData {
         return when (this) {
             is ItemTagData -> this
+            is ItemTagList -> this
+            is ItemTag -> this
             is Byte -> ItemTagData(this)
             is Short -> ItemTagData(this)
             is Int -> ItemTagData(this)
@@ -54,13 +70,19 @@ object ItemUtils {
             is Double -> ItemTagData(this)
             is ByteArray -> ItemTagData(this)
             is IntArray -> ItemTagData(this)
-            is String -> this.cast().toItemTagData()
+            is String -> this.castToItemTagData()
             is List<*> -> {
-                this.map { it?.cast() ?: it }
+                val list = this.map { it?.cast() ?: it }
                 when {
-                    this.all { it is Byte } -> ByteArray(this.size){ this[it] as Byte }.toItemTagData()
-                    this.all { it is Int } -> IntArray(this.size){ this[it] as Int }.toItemTagData()
-                    else -> translateList(ItemTagList(), this)
+                    list.all { it is Byte } -> ByteArray(list.size){ list[it] as Byte }.toItemTagData()
+                    list.all { it is Int } -> IntArray(list.size){ list[it] as Int }.toItemTagData()
+                    else -> {
+                        val itemTagList = ItemTagList()
+                        for (obj in list) {
+                            obj?.toItemTagData()?.let { itemTagList.add(it) }
+                        }
+                        itemTagList
+                    }
                 }
             }
             is HashMap<*, *> -> ItemTagData(this.toItemTag())
