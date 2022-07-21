cd01c53138a705a528039795e94f9ff6134d5cf5
Neige <2468629609@qq.com>
2022-07-21 20:45:22 +0800

    更改权重节点逻辑，适应大数字情况

--- src/main/kotlin/pers/neige/neigeitems/section/impl/WeightParser.kt
+++ src/main/kotlin/pers/neige/neigeitems/section/impl/WeightParser.kt
@@ -5,6 +5,7 @@ import org.bukkit.configuration.ConfigurationSection
 import org.bukkit.configuration.file.YamlConfiguration
 import pers.neige.neigeitems.section.SectionParser
 import pers.neige.neigeitems.utils.SectionUtils.parseSection
+import java.math.BigDecimal
 
 // 权重节点解析器
 object WeightParser : SectionParser() {
@@ -16,7 +17,8 @@ object WeightParser : SectionParser() {
         player: OfflinePlayer?, sections:
         ConfigurationSection?
     ): String? {
-        val strings = ArrayList<String>()
+        val info = HashMap<String, BigDecimal>()
+        val total = BigDecimal(0)
         // 加载所有参数并遍历
         data.getStringList("values").forEach {
             val value = it.toString().parseSection(cache, player, sections)
@@ -24,20 +26,34 @@ object WeightParser : SectionParser() {
             when (val index = value.indexOf("::")) {
                 // 无权重, 直接记录
                 -1 -> {
-                    strings.add(value)
+                    info[value]?.add(BigDecimal(1)) ?: let { info[value] = BigDecimal(1) }
+                    total.add(BigDecimal(1))
                 }
                 // 有权重, 根据权重大小进行记录
                 else -> {
-                    val weight = value.substring(0, index).toIntOrNull() ?: 1
+                    val weight = value.substring(0, index).toLongOrNull() ?: 1
                     val string = value.substring(index+2, value.length)
-                    for (i in 1..weight) strings.add(string)
+                    info[string]?.add(BigDecimal(weight)) ?: let { info[string] = BigDecimal(weight) }
+                    total.add(BigDecimal(weight))
                 }
             }
         }
         // 根据最后的记录值进行字符随机
         return when {
-            strings.isEmpty() -> null
-            else -> strings[(0 until strings.size).random()].parseSection(cache, player, sections)
+            info.isEmpty() -> null
+            else -> {
+                val random = BigDecimal(Math.random().toString()).multiply(total)
+                val current = BigDecimal(0)
+                var result: String? = null
+                for ((key, value) in info) {
+                    current.add(value)
+                    if (random <= current) {
+                        result = key.parseSection(cache, player, sections)
+                        break
+                    }
+                }
+                result
+            }
         }
     }
 
