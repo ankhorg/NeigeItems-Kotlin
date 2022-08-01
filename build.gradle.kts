a6a19cab7901df17930710d8976707037887080b
Neige <2468629609@qq.com>
2022-08-01 20:07:10 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,13 +36,13 @@ taboolib {
 repositories {
     mavenLocal()
     mavenCentral()
-    maven { url = uri("https://repo.tabooproject.org/storages/public/releases") }
     maven { url = uri("https://hub.spigotmc.org/nexus/content/repositories/public") }
     maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
     maven { url = uri("https://repo.dmulloy2.net/repository/public/") }
     maven { url = uri("https://repo.extendedclip.com/content/repositories/placeholderapi/") }
     maven { url = uri("https://mvn.lumine.io/repository/maven-public") }
     maven { url = uri("https://jitpack.io") }
+    maven { url = uri("https://repo.tabooproject.org/storages/public/releases") }
 }
 
 dependencies {
