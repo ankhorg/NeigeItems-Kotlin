c1b086f08e175694e8cd72c7718b314fbfd75430
Neige <72865616+Neige7@users.noreply.github.com>
2023-04-09 10:51:08 +0800

    尝试修复物品掉落颜色导致BC端踢出玩家的问题

--- src/main/kotlin/pers/neige/neigeitems/item/color/impl/ItemColorProtocol.kt
+++ src/main/kotlin/pers/neige/neigeitems/item/color/impl/ItemColorProtocol.kt
@@ -11,11 +11,13 @@ import com.comphenix.protocol.reflect.FieldAccessException
 import com.comphenix.protocol.utility.MinecraftReflection
 import com.comphenix.protocol.utility.MinecraftVersion
 import com.comphenix.protocol.wrappers.WrappedChatComponent
+import org.bukkit.Bukkit
 import org.bukkit.ChatColor
 import org.bukkit.Material
 import org.bukkit.entity.Item
 import org.bukkit.entity.Player
 import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.scoreboard.Scoreboard
 import pers.neige.neigeitems.NeigeItems.plugin
 import pers.neige.neigeitems.item.color.ItemColor
 import pers.neige.neigeitems.utils.PlayerUtils.getMetadataEZ
@@ -48,85 +50,105 @@ class ItemColorProtocol : ItemColor() {
         }
     }
 
-    private val teamPackets = HashMap<String, PacketContainer>().also { teamPackets ->
-        for ((id, color) in colors) {
-            // 创建Team数据包
-            val packet = PacketContainer(PacketType.Play.Server.SCOREBOARD_TEAM)
-            // 设置队伍ID
-            packet.strings.write(0, "NIProtocol-$color")
-            when (version) {
-                // 1.12版本, 设置前缀即可显示颜色
-                12 -> {
-                    // 设置数据包类型为"创建Team"
-                    packet.integers.write(1, 0)
-                    // 设置队伍前缀
-                    packet.strings.write(2, color.toString())
-                }
-                // 1.13-1.16, 设置颜色即可显示颜色
-                13 -> {
-                    // 设置数据包类型为"创建Team"
-                    packet.integers.write(0, 0)
-                    // 设置队伍颜色
-                    packet.getEnumModifier(ChatColor::class.java, enumChatFormatClass).write(0, color)
-                }
-                // 1.17+, 重点仍旧在设置颜色, 但是创建数据包时PacketPlayOutScoreboardTeam$b是没有默认值的, 需要一个个手动填满
-                else -> {
-                    // 设置数据包类型为"创建Team"
-                    packet.integers.write(0, 0)
-                    // 获取队伍信息
-                    val internalStructure = packet.optionalStructures.read(0).get()
-                    // 设置队伍颜色
-                    internalStructure.getEnumModifier(ChatColor::class.java, enumChatFormatClass).write(0, color)
-                    // 设置展示名
-                    internalStructure.chatComponents.write(0, WrappedChatComponent.fromText("NIProtocol-$color"))
-                    // 设置前缀
-                    internalStructure.chatComponents.write(1, WrappedChatComponent.fromText(color.toString()))
-                    // 设置后缀
-                    internalStructure.chatComponents.write(2, WrappedChatComponent.fromText(""))
-                    // 设置名牌可见性
-                    internalStructure.strings.write(0, "always")
-                    // 设置TeamPush
-                    internalStructure.strings.write(1, "always")
-                    // 设置友军选项
-                    internalStructure.integers.write(0, 3)
-                }
-            }
-            // 存储数据包
-            teamPackets[id] = packet
-        }
-    }
+    private val checkedScoreboard = HashSet<Scoreboard>()
+
+//    private val teamPackets = HashMap<String, PacketContainer>().also { teamPackets ->
+//        for ((id, color) in colors) {
+//            // 创建Team数据包
+//            val packet = PacketContainer(PacketType.Play.Server.SCOREBOARD_TEAM)
+//            // 设置队伍ID
+//            packet.strings.write(0, "NIProtocol-$color")
+//            when (version) {
+//                // 1.12版本, 设置前缀即可显示颜色
+//                12 -> {
+//                    // 设置数据包类型为"创建Team"
+//                    packet.integers.write(1, 0)
+//                    // 设置队伍前缀
+//                    packet.strings.write(2, color.toString())
+//                }
+//                // 1.13-1.16, 设置颜色即可显示颜色
+//                13 -> {
+//                    // 设置数据包类型为"创建Team"
+//                    packet.integers.write(0, 0)
+//                    // 设置队伍颜色
+//                    packet.getEnumModifier(ChatColor::class.java, enumChatFormatClass).write(0, color)
+//                }
+//                // 1.17+, 重点仍旧在设置颜色, 但是创建数据包时PacketPlayOutScoreboardTeam$b是没有默认值的, 需要一个个手动填满
+//                else -> {
+//                    // 设置数据包类型为"创建Team"
+//                    packet.integers.write(0, 0)
+//                    // 获取队伍信息
+//                    val internalStructure = packet.optionalStructures.read(0).get()
+//                    // 设置队伍颜色
+//                    internalStructure.getEnumModifier(ChatColor::class.java, enumChatFormatClass).write(0, color)
+//                    // 设置展示名
+//                    internalStructure.chatComponents.write(0, WrappedChatComponent.fromText("NIProtocol-$color"))
+//                    // 设置前缀
+//                    internalStructure.chatComponents.write(1, WrappedChatComponent.fromText(color.toString()))
+//                    // 设置后缀
+//                    internalStructure.chatComponents.write(2, WrappedChatComponent.fromText(""))
+//                    // 设置名牌可见性
+//                    internalStructure.strings.write(0, "always")
+//                    // 设置TeamPush
+//                    internalStructure.strings.write(1, "always")
+//                    // 设置友军选项
+//                    internalStructure.integers.write(0, 3)
+//                }
+//            }
+//            // 存储数据包
+//            teamPackets[id] = packet
+//        }
+//    }
 
     /**
      * 根据玩家当前计分板进行Team初始化
      * 玩家的计分板不一定一成不变,
      * 可能刚进服时玩家是主计分板,
      * 过一段时间其他插件又根据需要切换了玩家的计分板.
-     * 这样就需要重新向玩家发送Team数据包,
-     * 不然玩家的客户端将认为之前的Team与玩家无关,
+     * 需要保证每个计分板内都存在对应的Team,
+     * 不然玩家的客户端将认为掉落物所在的Team与玩家无关,
      * 从而导致掉落物只发出白色光效.
      *
      * @param player 待操作玩家
-     * @param force 是否强制初始化
      */
-    fun initTeam(player: Player, force: Boolean = false) {
+    fun initTeam(player: Player) {
+        if (!checkedScoreboard.contains(player.scoreboard)) {
+            for ((id, color) in colors) {
+                // 注册Team
+                var team = player.scoreboard.getTeam("NIProtocol-$color")
+                team?.unregister()
+                team = player.scoreboard.registerNewTeam("NIProtocol-$color")
+                // 1.13+设置color即可改变光效发光颜色
+                team.color = color
+                // 1.12-需要给prefix设置颜色才能改变发光颜色
+                team.prefix = color.toString()
+            }
+            checkedScoreboard.add(player.scoreboard)
+        }
+
+
+        // 因为某些原因, BungeeCord会将玩家收到的"创建Team"数据包转化为实体, 导致BC端Team重复创建, 产生报错并踢出玩家
+        // 所以将策略从"向玩家发送创建Team数据包, 后续发送向对应Team添加实体的数据包"
+        // 改为"在所有Scoreboard中真实创建对应的Team, 后续发送向对应Team添加实体的数据包"
+
         // 每次发包后会通过Metadata记录玩家当前所处计分板,
         // 没有相关Metadata说明从未初始化, Metadata不符说明计分板发生了切换, 都需要重新进行Team初始化.
         // force代表强制初始化, 用于玩家登录时进行操作
         // 直接scoreboard.toString()可以暴露当前Scoreboard的指针, 从而正确判断是否为同一个计分板.
-        if (force || !player.hasMetadata("NI-TeamScoreboard") || (player.getMetadataEZ("NI-TeamScoreboard", "String", "") as String) != player.scoreboard.toString()) {
-            // 初始化前通过Metadata记录当前计分板
-            player.setMetadataEZ("NI-TeamScoreboard", player.scoreboard.toString())
-            for ((id, packet) in teamPackets) {
-                // 发送数据包
-                protocolManager.sendServerPacket(player, packet)
-            }
-        }
+//        if (force || !player.hasMetadata("NI-TeamScoreboard") || (player.getMetadataEZ("NI-TeamScoreboard", "String", "") as String) != player.scoreboard.toString()) {
+//            // 初始化前通过Metadata记录当前计分板
+//            player.setMetadataEZ("NI-TeamScoreboard", player.scoreboard.toString())
+//            for ((id, packet) in teamPackets) {
+//                // 发送数据包
+//                protocolManager.sendServerPacket(player, packet)
+//            }
+//        }
     }
 
     init {
         // 玩家登录时根据玩家当前计分板进行Team初始化
         registerBukkitListener(PlayerJoinEvent::class.java, EventPriority.NORMAL, false) {
-            initTeam(it.player, true)
+            initTeam(it.player)
         }
 
         // 用于设置物品发光颜色
