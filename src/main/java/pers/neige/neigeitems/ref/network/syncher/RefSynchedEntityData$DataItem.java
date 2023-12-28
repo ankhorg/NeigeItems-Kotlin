1902edd2508cfca078b4ad5d59e81a3f74258366
Neige <2468629609@qq.com>
2023-12-28 11:13:29 +0800

    v1.16.6 update
    修复"修改EntityMetadata数据包中实体自定义名称"功能在1.12.2失效的问题

--- src/main/java/pers/neige/neigeitems/ref/network/syncher/RefSynchedEntityData$DataItem.java
+++ src/main/java/pers/neige/neigeitems/ref/network/syncher/RefSynchedEntityData$DataItem.java
@@ -6,6 +6,6 @@ import org.inksnow.ankhinvoke.comments.HandleBy;
 @HandleBy(reference = "net/minecraft/server/v1_12_R1/DataWatcher$Item", predicates = "craftbukkit_version:[v1_12_R1,v1_17_R1)")
 public class RefSynchedEntityData$DataItem {
     @HandleBy(reference = "Lnet/minecraft/network/syncher/DataWatcher$Item;a()Lnet/minecraft/network/syncher/DataWatcherObject;", predicates = "craftbukkit_version:[v1_17_R1,)")
-    @HandleBy(reference = "Lnet/minecraft/server/v1_12_R1/DataWatcherObject;b()Lnet/minecraft/server/v1_12_R1/DataWatcherSerializer;", predicates = "craftbukkit_version:[v1_12_R1,v1_17_R1)")
+    @HandleBy(reference = "Lnet/minecraft/server/v1_12_R1/DataWatcher$Item;a()Lnet/minecraft/server/v1_12_R1/DataWatcherObject;", predicates = "craftbukkit_version:[v1_12_R1,v1_17_R1)")
     public native RefEntityDataAccessor getAccessor();
 }
