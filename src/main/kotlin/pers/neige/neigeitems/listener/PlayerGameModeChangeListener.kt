8acfe3cc165eee265d5cff8ecb2669b189327647
Neige <2468629609@qq.com>
2024-02-24 14:19:12 +0800

    v1.16.35 update
    添加切换游戏模式后自动更新物品变量的功能

---
+++ src/main/kotlin/pers/neige/neigeitems/listener/PlayerGameModeChangeListener.kt
@@ -0,0 +1,22 @@
+package pers.neige.neigeitems.listener
+
+import org.bukkit.GameMode
+import org.bukkit.event.player.PlayerGameModeChangeEvent
+import pers.neige.neigeitems.annotation.Listener
+import pers.neige.neigeitems.manager.ConfigManager
+import pers.neige.neigeitems.utils.SchedulerUtils.syncLater
+
+object PlayerGameModeChangeListener {
+    @JvmStatic
+    @Listener
+    fun listener(event: PlayerGameModeChangeEvent) {
+        val player = event.player
+        if (player.gameMode == GameMode.CREATIVE || event.newGameMode == GameMode.CREATIVE) {
+            if (ConfigManager.config.getBoolean("ItemPlaceholder.enable")) {
+                syncLater(1) {
+                    player.updateInventory()
+                }
+            }
+        }
+    }
+}
