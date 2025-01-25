e814435851507347f5fe8cd748184a92ced9ae83
Neige <2468629609@qq.com>
2025-01-25 17:50:20 +0800

    v1.20.69 update
    优化指令写法及权限判断逻辑。

--- src/main/kotlin/pers/neige/neigeitems/command/Command.kt
+++ src/main/kotlin/pers/neige/neigeitems/command/Command.kt
@@ -4,6 +4,8 @@ import com.mojang.brigadier.CommandDispatcher
 import com.mojang.brigadier.ParseResults
 import com.mojang.brigadier.StringReader
 import com.mojang.brigadier.builder.LiteralArgumentBuilder
+import com.mojang.brigadier.context.CommandContextBuilder
+import com.mojang.brigadier.tree.LiteralCommandNode
 import org.bukkit.command.CommandSender
 import org.bukkit.command.PluginCommand
 import pers.neige.neigeitems.NeigeItems
@@ -28,6 +30,19 @@ object Command {
         register(command)
         mapping[command.literal.lowercase()] = command.literal
         return this
+    }
+
+    private fun getPermission(context: CommandContextBuilder<*>): String {
+        val permission = StringBuilder("$COMMAND_NAME.command")
+        for (node in context.nodes) {
+            if (node.node is LiteralCommandNode) {
+                permission.append(".")
+                permission.append(node.node.name.lowercase())
+            } else {
+                break
+            }
+        }
+        return permission.toString()
     }
 
     @Awake
@@ -66,19 +81,9 @@ object Command {
             aliases = arrayListOf("ni")
             permission = "$COMMAND_NAME.command"
             setExecutor { sender, _, _, args ->
-                val lowercaseKey = args.getOrNull(0)?.lowercase()
-                if (mapping.containsKey(lowercaseKey)) {
-                    if (!sender.hasPermission("$COMMAND_NAME.command.$lowercaseKey")) {
-                        sender.sendLang(
-                            "Messages.insufficientPermissions",
-                            mapOf(Pair("{permission}", "$COMMAND_NAME.command.$lowercaseKey"))
-                        )
-                        return@setExecutor true
-                    }
-                    args[0] = mapping[lowercaseKey]
-                } else {
-                    help(sender)
-                    return@setExecutor true
+                val arg0 = args.getOrNull(0)?.lowercase()
+                if (mapping.containsKey(arg0)) {
+                    args[0] = mapping[arg0]
                 }
                 val parse: ParseResults<CommandSender> = dispatcher.parse(args.joinToString(" "), sender)
                 if (parse.reader.canRead()) {
@@ -88,21 +93,30 @@ object Command {
                         return@setExecutor true
                     }
                 }
+                if (parse.context.nodes.isEmpty()) {
+                    help(sender)
+                    return@setExecutor true
+                }
+                val permission = getPermission(parse.context)
+                if (!sender.hasPermission(permission)) {
+                    sender.sendLang(
+                        "Messages.insufficientPermissions",
+                        mapOf(Pair("{permission}", permission))
+                    )
+                    return@setExecutor true
+                }
                 return@setExecutor dispatcher.execute(parse) > 0
             }
             setTabCompleter { sender, _, _, args ->
                 val lowercaseKey = args.getOrNull(0)?.lowercase()
                 if (mapping.containsKey(lowercaseKey)) {
-                    if (!sender.hasPermission("$COMMAND_NAME.command.$lowercaseKey")) {
-                        sender.sendLang(
-                            "Messages.insufficientPermissions",
-                            mapOf(Pair("{permission}", "$COMMAND_NAME.command.$lowercaseKey"))
-                        )
-                        return@setTabCompleter arrayListOf()
-                    }
                     args[0] = mapping[lowercaseKey]
                 }
                 val parse: ParseResults<CommandSender> = dispatcher.parse(args.joinToString(" "), sender)
+                val permission = getPermission(parse.context)
+                if (!sender.hasPermission(permission)) {
+                    return@setTabCompleter ArrayList()
+                }
                 return@setTabCompleter dispatcher.getCompletionSuggestions(parse).get().list.map { it.text }
             }
             CommandUtils.getCommandMap().register(this.name, this)
