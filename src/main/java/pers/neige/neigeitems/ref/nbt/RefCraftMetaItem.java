9b64390e23fc7bf57a9aacd3eb8683256b0a3559
Neige <2468629609@qq.com>
2026-07-14 14:22:04 +0800

    v1.21.156 update
    修复1.20.5-1.21.11版本无法正常启动的bug。

--- src/main/java/pers/neige/neigeitems/ref/nbt/RefCraftMetaItem.java
+++ src/main/java/pers/neige/neigeitems/ref/nbt/RefCraftMetaItem.java
@@ -81,9 +81,9 @@ public final class RefCraftMetaItem {
     @HandleBy(reference = "Lorg/bukkit/craftbukkit/CAN_DESTROY/inventory/CraftMetaItem;CAN_PLACE_ON:Lorg/bukkit/craftbukkit/v1_18_R1/inventory/CraftMetaItem$ItemMetaKey;", useAccessor = true, predicates = "craftbukkit_version:[v1_18_R1,v1_20_R3]")
     // "CanPlaceOn" 1.18
     public static final RefItemMetaKey CAN_PLACE_ON = null;
-    @HandleBy(reference = "Lorg/bukkit/craftbukkit/v1_12_R1/inventory/CraftMetaItem;HANDLED_TAGS:Ljava/util/Set;", useAccessor = true, predicates = "craftbukkit_version:[v1_12_R1,)")
+    @HandleBy(reference = "Lorg/bukkit/craftbukkit/v1_12_R1/inventory/CraftMetaItem;HANDLED_TAGS:Ljava/util/Set;", useAccessor = true, predicates = "craftbukkit_version:[v1_12_R1,v1_20_R4)")
     public static Set<String> HANDLED_TAGS;
-    @HandleBy(reference = "Lorg/bukkit/craftbukkit/v1_12_R1/inventory/CraftMetaItem;unhandledTags:Ljava/util/Map;", useAccessor = true, predicates = "craftbukkit_version:[v1_12_R1,)")
+    @HandleBy(reference = "Lorg/bukkit/craftbukkit/v1_12_R1/inventory/CraftMetaItem;unhandledTags:Ljava/util/Map;", useAccessor = true, predicates = "craftbukkit_version:[v1_12_R1,v1_20_R4)")
     public Map<String, RefNbtBase> unhandledTags;
 
     @HandleBy(reference = "Lorg/bukkit/craftbukkit/v1_17_R1/inventory/CraftMetaItem;applyToItem(Lnet/minecraft/nbt/CompoundTag;)V", useAccessor = true, predicates = "craftbukkit_version:[v1_17_R1,)")
